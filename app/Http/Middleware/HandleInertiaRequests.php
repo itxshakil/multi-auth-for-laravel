dfb3fd97b0b517ec11700d1c64defea2952bee97
Shakil Alam <itxshakil@gmail.com>
2023-02-15 00:07:18 +0530

    Send logged admin with request shared data
    Signed-off-by: Shakil Alam <itxshakil@gmail.com>

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,6 +33,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
+                'admin' => $request->user('admin'),
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
